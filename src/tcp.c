95c5307044761aca068c99a9808bef15372178ff
michal-jewczuk <michal.jewczuk@outlook.com>
2026-05-30 14:33:42 +0200

    Add guard for port > 8080

--- src/tcp.c
+++ src/tcp.c
@@ -2,6 +2,10 @@
 #include "main.h"
 
 server_status_e bind_tcp_port(tcp_server *server, int port) {
+    if (port > 8080) {
+        return SERVER_BIND_ERROR;
+    }
+
     memset(server, 0, sizeof(*server));
     server->socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server->socket_fd == -1) {
